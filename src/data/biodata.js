963a03e139757c772a657ace072c584ff7986efd
dikiwahyudin <74003372+dixs-bot@users.noreply.github.com>
2025-11-24 23:49:20 +0700

    Update biodata.js

--- src/data/biodata.js
+++ src/data/biodata.js
@@ -1,9 +1,9 @@
 export const biodata = [
   { id: 1, label: "Nama Lengkap", value: "Diki Wahyudin" },
-  { id: 2, label: "TTL", value: "Kuningan, 10 Mei 2004" },
-  { id: 3, label: "Alamat", value: "Ciawi, Kab. Kuningan, Jawa Barat" },
+  { id: 2, label: "TTL", value: "Tasikmalaya, 17 September 1999" },
+  { id: 3, label: "Alamat", value: "cimahi tengah, Kel. padasuka, Jawa Barat" },
   { id: 4, label: "Jenis Kelamin", value: "Laki-laki" },
-  { id: 5, label: "Status", value: "Belum menikah" },
-  { id: 6, label: "E-mail", value: "contoh@gmail.com" },
+  { id: 5, label: "Status", value: "Menikah" },
+  { id: 6, label: "E-mail", value: "dikiwahyudin2020@gmail.com" },
   { id: 7, label: "Telepon / WA", value: "0851-8997-6233" }
 ];
