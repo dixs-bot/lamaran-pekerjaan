92fdc96862d291dee8821ad7af4e9d3d3158df36
dikiwahyudin <74003372+dixs-bot@users.noreply.github.com>
2025-11-24 19:47:37 +0700

    Update experience.js

--- src/data/experience.js
+++ src/data/experience.js
@@ -1,17 +1,17 @@
 export const experiences = [
   {
     id: 1,
-    company: "[Nama Perusahaan 1]",
-    period: "[Tahun Masuk] - [Tahun Keluar]",
-    role: "[Jabatan]",
-    description: "[Ringkasan tugas utama singkat].",
+    company: "[PT.NUSANTARA JAYA SENTOSA]",
+    period: "[2023] - [2024]",
+    role: "[Web Developer]",
+    description: "[Membuat dan menyusun web untuk sales Serta membuat project kalkulator simulasi untuk tiap tim sales].",
   },
   {
     id: 2,
-    company: "[Nama Perusahaan 2]",
-    period: "[Tahun Masuk] - [Tahun Keluar]",
-    role: "[Jabatan]",
-    description: "[Deskripsi ringkas tugas & kontribusi].",
+    company: "[PT.PRIMANUSA DUTAMAS]",
+    period: "[2022] - [2023]",
+    role: "[WEB Developer]",
+    description: "[Membangun dan membuat ulang desain web yang responsive serta mengintegrasikan srtuktur administrasi antar perusahaan].",
   },
   // Kalau mau nambah lagi, tinggal tambah object baru di sini
 ];
